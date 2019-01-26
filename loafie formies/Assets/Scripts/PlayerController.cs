b01ad54bce9d3cb80c1461f9bc876eebd038b45c
Devon Blanton <lenzo1gaming@gmail.com>
2019-01-25 22:12:37 -0600

    update player

--- loafie formies/Assets/Scripts/PlayerController.cs
+++ loafie formies/Assets/Scripts/PlayerController.cs
@@ -10,12 +10,17 @@ public class PlayerController : MonoBehaviour
 
     public int friction;
 
+    public float objectScale;
+
     private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        this.transform.localScale = new Vector3(objectScale, objectScale, 1);
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb2d.drag = friction;
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -24,13 +29,21 @@ public class PlayerController : MonoBehaviour
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
+        if(moveHorizontal > 0)
+        {
+            this.transform.localScale = new Vector3(-objectScale, objectScale, 1);
+        }
+        else if(moveHorizontal < 0)
+        {
+            this.transform.localScale = new Vector3(objectScale, objectScale, 1);
+        }
+
         //Store the current vertical input in the float moveVertical.
         float moveVertical = Input.GetAxis("Vertical");
 
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical/2);
 
-        rb2d.angularDrag = friction;
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
