04df7e01aa6f9aea70481354f110a4a7c78e7706
Devon Blanton <lenzo1gaming@gmail.com>
2019-01-26 17:17:10 -0600

    update camera and friend

--- loafie formies/Assets/Scripts/Friend.cs
+++ loafie formies/Assets/Scripts/Friend.cs
@@ -8,15 +8,30 @@ public class Friend : MonoBehaviour
     public Vector3 offset;
     public int followDistance;
 
+    private float facing;
+
+    private void Start()
+    {
+        facing = transform.parent.localScale.x;
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         target = other.transform;
-        Debug.Log("Trigger");
     }
 
     private void FixedUpdate()
     {
         if (target && (Vector3.Distance(target.position, transform.position) > followDistance)) {
+            if (target.position.x > transform.position.x)
+            {
+                transform.parent.localScale = new Vector3(-facing, Mathf.Abs(facing), 1);
+            }
+            else if (target.position.x < transform.position.x)
+            {
+                transform.parent.localScale = new Vector3(facing, Mathf.Abs(facing), 1);
+            }
             Vector3 desiredPosition = target.position + offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
             transform.parent.position = smoothedPosition;
