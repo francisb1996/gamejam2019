4fec58e298031d528ece0e07953c572c5ecfb413
Brad Francis <Brad.Francis@daugherty.com>
2019-01-25 22:12:14 -0600

    starting acid

---
+++ loafie formies/Assets/Scripts/Spread.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spread : MonoBehaviour
+{
+    int rate
+    int density
+    Sprite sprite
+    Color color
+    private SpriteRenderer sr
+
+    void Awake()
+    {
+        sr = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+        sr.color = color
+        sr.sprite = SpriteRenderer
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
