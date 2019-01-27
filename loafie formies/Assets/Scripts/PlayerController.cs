c95fbdf932820518ef62e2431accf27e5777b454
Devon Blanton <lenzo1gaming@gmail.com>
2019-01-26 19:56:30 -0600

    loafie rotates

--- loafie formies/Assets/Scripts/PlayerController.cs
+++ loafie formies/Assets/Scripts/PlayerController.cs
@@ -17,11 +17,14 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
+    private float maxRotation = 15;
+    private float rotationSpeed = 8;
+    private float returnSpeed = 12;
 
     void Start()
     {
         isSpeedBuffed = false;
-        this.transform.localScale = new Vector3(objectScale, objectScale, 1);
+        transform.localScale = new Vector3(objectScale, objectScale, 1);
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -31,18 +34,76 @@ public class PlayerController : MonoBehaviour
     {
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
 
-        if(moveHorizontal > 0)
+        //Turn based on moving direction
+        if (moveHorizontal > 0)
         {
-            this.transform.localScale = new Vector3(-objectScale, objectScale, 1);
+            transform.localScale = new Vector3(-objectScale, objectScale, 1);
         }
         else if(moveHorizontal < 0)
         {
-            this.transform.localScale = new Vector3(objectScale, objectScale, 1);
+            transform.localScale = new Vector3(objectScale, objectScale, 1);
         }
 
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
+        bool facingLeft = transform.localScale.x > 0;
+        bool facingRight = transform.localScale.x < 0;
+
+        bool movingUp = moveVertical > 0;
+        bool movingDown = moveVertical < 0;
+        bool notMoving = moveVertical == 0;
+
+
+        Vector3 clockwise = Vector3.back;
+        Vector3 counterClockwise = Vector3.forward;
+
+        float currentAngle = getCurrentAngle();
+
+        //Rotate based on moving direction
+        if (facingRight)
+        {
+            if(movingUp &&
+               currentAngle < maxRotation)
+            {
+                transform.Rotate(counterClockwise * Time.deltaTime * rotationSpeed);
+            }
+            else if(movingDown &&
+                    currentAngle > -maxRotation)
+            {
+                transform.Rotate(clockwise * Time.deltaTime * rotationSpeed);
+            }
+        }
+        else if (facingLeft)
+        {
+            if (movingUp &&
+               currentAngle > -maxRotation)
+            {
+                transform.Rotate(clockwise * Time.deltaTime * rotationSpeed);
+            }
+            else if (movingDown &&
+                    currentAngle < maxRotation)
+            {
+                transform.Rotate(counterClockwise * Time.deltaTime * rotationSpeed);
+            }
+        }
+
+        //Return to 0 rotation when not moving
+        if(notMoving)
+        {
+            if (currentAngle > 0.14)
+            {
+                transform.Rotate(clockwise * Time.deltaTime * returnSpeed);
+            }
+            else if (currentAngle < -0.14)
+            {
+                transform.Rotate(counterClockwise * Time.deltaTime * returnSpeed);
+            }
+            else
+            {
+                transform.rotation = new Quaternion(0,0,0,0);
+            }
+        }
 
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical/2);
@@ -50,6 +111,19 @@ public class PlayerController : MonoBehaviour
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
+    }
+
+    float getCurrentAngle()
+    {
+        float angle = transform.eulerAngles.z;
+        if(angle > 150)
+        {
+            return angle - 360;
+        }
+        else
+        {
+            return angle;
+        }
     }
 
     void Update()
